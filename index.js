523a269281a6f89f1e8dc90f4f4b642ccc5e4cdd
YUIAOI <qwe153246@gmail.com>
2020-08-28 21:14:14 +0800

    Rest/Spread Properties

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 "use strict";
-var _loop_1 = function (i) {
-    console.log(i);
-    setTimeout(function () {
-        console.log('這執行第' + i + '次');
-    }, 10);
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-for (var i = 0; i < 10; i++) {
-    _loop_1(i);
-}
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+var nestedObject = {
+    prop: 'Hello',
+    child: {
+        prop1: 123,
+        prop2: false
+    }
+};
+var obj1 = { hello: 'World' };
+var obj2 = __assign(__assign({}, obj1), { goodbye: 'Cruel World' });
+console.log(nestedObject);
+console.log(obj1);
+console.log(obj2);
+var _a = { x: 1, y: 2, a: 3, b: 4 }, x = _a.x, y = _a.y, z = __rest(_a, ["x", "y"]);
+var n = __assign({ x: x, y: y }, z);
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(n);
--- index.ts
+++ index.ts
@@ -1,6 +1,18 @@
-for (let i = 0; i < 10; i++) {
-    console.log(i);
-    setTimeout(function () {
-        console.log('這執行第' + i + '次');
-    }, 10);
-}
+let nestedObject = {
+    prop: 'Hello',
+    child: {
+        prop1: 123,
+        prop2: false
+    }
+};
+let obj1 = { hello: 'World' };
+let obj2 = { ...obj1, goodbye: 'Cruel World' };
+console.log(nestedObject);
+console.log(obj1);
+console.log(obj2);
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+let n = { x, y, ...z };
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(n);
